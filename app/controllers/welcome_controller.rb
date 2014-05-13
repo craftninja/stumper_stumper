84d76ea8e7cbafb4214fd2d4becfe499d70a4aa9
Jeff Dean <jeff@galvanize.it>
2014-05-12 21:28:42 -0600

    Added about page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,4 +4,16 @@ class WelcomeController < ApplicationController
     @games = Game.all
   end
 
+  def about
+    @examples = [
+      [247, 126, [0, 1]],
+      [247, 247, [3, 0]],
+      [247, 222, [1, 0]],
+      [247, 737, [1, 0]],
+      [247, 724, [0, 3]],
+      [242, 223, [1, 1]],
+      [74372, 43177, [1, 3]]
+    ]
+  end
+
 end
