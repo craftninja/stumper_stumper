c0c34b865d93bc13a492ed4bdf2dcd5584c0b4a9
Jeff Dean <jeff@galvanize.it>
2014-05-12 21:22:21 -0600

    added link to header - stub for about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   root 'welcome#index'
+  get '/about' => 'welcome#about', as: :about
 
   get '/games/new/:level' => 'games#new', as: :new_game
   get '/games/:id' => 'games#show', as: :game
